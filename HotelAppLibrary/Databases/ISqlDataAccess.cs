1ed61b9aac04ff8dedd9f6590b8604379d4133b6
Robertas Suminskas <robertas.suminskas@gmail.com>
2023-11-26 18:13:21 +0000

    M09c19 - END  Created Load and Save Data from database, created Interface as well.

---
+++ HotelAppLibrary/Databases/ISqlDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace HotelAppLibrary.Databases
+{
+    public interface ISqlDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatment,
+                               U parameters,
+                               string connectionStringName,
+                               bool isStoreProcedure = false);
+        void SaveData<T>(string sqlStatment,
+                         T parameters,
+                         string connectionStringName,
+                         bool isStoreProcedure = false);
+    }
+}
--- HotelAppLibrary/Databases/SqlDataAccess.cs
+++ HotelAppLibrary/Databases/SqlDataAccess.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Text;
 using Dapper;
 using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
 
 namespace HotelAppLibrary.Databases
 {
-    public class SqlDataAccess
+    public class SqlDataAccess : ISqlDataAccess
     {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // isStoredProcedure is a flag to tell the method if the sqlStatment is a stored procedure or not. SQL Lite doesn't support stored procedures
+        // Detail Summary of the method.
+        public List<T> LoadData<T, U>(string sqlStatment,
+                                       U parameters,
+                                       string connectionStringName,
+                                       bool isStoredProcedure = false )
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatment, parameters, commandType: commandType).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatment,
+                                T parameters,
+                                string connectionStringName,
+                                bool isStoredProcedure = false )
+
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sqlStatment, parameters, commandType: commandType);
+            }
+        }
+
     }
 }
