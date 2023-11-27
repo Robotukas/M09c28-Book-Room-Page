17f8818f25a6dccd7cbfd9e8c18eb72a00f90096
Robertas Suminskas <robertas.suminskas@gmail.com>
2023-11-27 21:52:54 +0000

    M09c21 - Created store procedures, created method BookGuest

---
+++ HotelAppDB/dbo/Stored Procedures/spGuests_Insert.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spGuests_Insert]
+	@firstName nvarchar(50),
+	@lastName nvarchar(50)
+AS
+begin
+	set nocount on;	/* prevents the "1 row affected" message */
+
+	if not exists (select * from dbo.Guests where FirstName = @firstName and LastName = @lastName)
+	begin
+		insert into dbo.Guests (FirstName, LastName)
+		values (@firstName, @lastName);
+	end
+
+	select top 1 [Id], [FirstName], [LastName]
+	from dbo.Guests 
+	where FirstName = @firstName and LastName = @lastName;
+end
--- HotelAppLibrary/Data/SqlData.cs
+++ HotelAppLibrary/Data/SqlData.cs
@@ -3,6 +3,7 @@ using HotelAppLibrary.Models;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HotelAppLibrary.Data
@@ -22,12 +23,69 @@ namespace HotelAppLibrary.Data
         {
             _db = db;
         }
+
+        /// <summary>
+        /// is designed to retrieve a list of available room types for a specified date range in a hotel 
+        /// management system. Here's a summary of the method:
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
         public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
         {
-            return _db.LoadData<RoomTypeModel, dynamic> ("dbo.spRoomType_GetAvailableTypes",
+            return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomType_GetAvailableTypes",
                                                   new { startDate, endDate }, // Parameters
                                                   connectionStringName,  // Connection string name to sql server
                                                   true);  // Is stored procedure
         }
+
+        /// <summary>
+        /// The BookGuest method encapsulates the process of booking a hotel room for a guest, starting
+        /// from guest registration to room booking. It uses a series of database operations to handle
+        /// different aspects of the booking process, ensuring that all necessary information is captured
+        /// and processed correctly. The method demonstrates a clear sequence of steps, maintaining
+        /// a separation of concerns and leveraging the database for critical operations.
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <param name="roomTypeId"></param>
+        public void BookGuest(string firstName,
+                              string lastName,
+                              DateTime startDate,
+                              DateTime endDate,
+                              int roomTypeId)
+        {
+            
+            GuestModel guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuests_Insert",
+                                                                 new {firstName, lastName },
+                                                                 connectionStringName,
+                                                                 true).First(); 
+
+            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomType_GetById where Id = @Id",
+                                                                          new { Id = roomTypeId },
+                                                                          connectionStringName,
+                                                                           false).First();
+
+            TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
+            
+            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms",
+                                                                              new { startDate, endDate, roomTypeId },
+                                                                              connectionStringName,
+                                                                              true);
+
+            _db.SaveData("dbo.spBookings_Insert",
+                         new 
+                         { 
+                             roomId = availableRooms.First().Id,
+                             guestId = guest.Id,
+                             startDate = startDate, 
+                             endDate = endDate,
+                             totalCost = timeStaying.Days * roomType.Price
+                         },
+                         connectionStringName,
+                         true);
+        }                               
     }
 }
