31ca8ef2150982318ecb44b3da16263ef99368d1
Robertas Suminskas <robertas.suminskas@gmail.com>
2023-12-16 10:32:36 +0000

    M09.27__12:19 - Created fields and labels in page "RoomSearch"

--- HotelApp.Web/Pages/RoomSearch.cshtml.cs
+++ HotelApp.Web/Pages/RoomSearch.cshtml.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace HotelApp.Web.Pages
 {
     public class RoomSearchModel : PageModel
     {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            return Page();
         }
     }
 }
